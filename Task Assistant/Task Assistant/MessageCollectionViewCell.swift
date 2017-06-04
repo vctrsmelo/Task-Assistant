ccc521cbf4ea1bc53c0f2142afc0b8844eca89bb
Victor S Melo <victor.contas@outlook.com>
2017-06-04 09:11:23 -0300

    chatview in development

--- Task Assistant/Task Assistant/MessageCollectionViewCell.swift
+++ Task Assistant/Task Assistant/MessageCollectionViewCell.swift
@@ -10,4 +10,43 @@ import UIKit
 
 class MessageCollectionViewCell: UICollectionViewCell {
     
+    private var message: Message!
+    
+    let messageMargin: CGFloat = 5.0
+    var userMessageXPosition: CGFloat!
+    var daveMessageXPosition: CGFloat!
+    @IBOutlet weak var textView: UITextView!
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    public func set(message: Message){
+        self.message = message
+        self.textView.text = message.text
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        self.userMessageXPosition = rect.size.width-textView.frame.width-self.messageMargin
+        
+        self.daveMessageXPosition =  self.messageMargin
+        self.textView.text = message.text
+        
+        var frame = textView.frame
+        frame.origin.x = (self.message.source == .User) ? self.userMessageXPosition : daveMessageXPosition
+
+        //align text horizontally
+        frame.origin.y = rect.origin.y+(self.frame.size.height/2.0) - (textView.frame.size.height/2.0)
+        print("originX: \(frame.origin.x) originY: \(frame.origin.y)")
+        textView.frame = frame
+        
+    }
+    
 }
