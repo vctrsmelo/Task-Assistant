ac2a221bff633ee265f81f283de042a2ec5574e5
Victor S Melo <victor.contas@outlook.com>
2017-06-03 22:55:16 -0300

    storyboard defined

---
+++ Task Assistant/Task Assistant/ChatCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  ChatCollectionView.swift
+//  Task Assistant
+//
+//  Created by Victor S Melo on 03/06/17.
+//  Copyright © 2017 Victor S Melo. All rights reserved.
+//
+
+import UIKit
+
+class ChatCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
