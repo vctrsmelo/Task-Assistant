0a1e08fc70ff72ce238f54e308c482b9f4bf28db
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2017-07-05 03:34:29 -0300

    Screen shows data from the test project

--- Task Assistant/Task Assistant/ActivityViewController.swift
+++ Task Assistant/Task Assistant/ActivityViewController.swift
@@ -13,10 +13,28 @@ class ActivityViewController: UIViewController {
 	var project : Project?
 	var task : Task?
 	
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var startDayMonthView: DayMonthView!
+	@IBOutlet weak var endDayMonthView: DayMonthView!
+	@IBOutlet weak var hiddenPickerView: UIView!
+	@IBOutlet weak var progressBar: UIProgressView!
+	@IBOutlet weak var workedHoursLabel: UILabel!
+	@IBOutlet weak var needHoursLabel: UILabel!
+	@IBOutlet weak var importanceLabel: UILabel!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		
+		if let currentProject = self.project {
+			self.titleLabel.text = currentProject.title
+			self.startDayMonthView.date = currentProject.startDate
+			self.endDayMonthView.date = currentProject.endDate
+			self.progressBar.setProgress(0, animated: true) // TODO: Get the current completion value
+			self.workedHoursLabel.text = "\(0) hours" // TODO: Use correct value here too
+			self.needHoursLabel.text = "\(currentProject.estimatedTime/60/60) hours"
+			self.importanceLabel.text = currentProject.priority.toString()
+		}
+		
         // Do any additional setup after loading the view.
     }
 
